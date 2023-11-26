244da3d6ffec54d7e507808f765efe17e89069fb
ekaterinakrylovao <145141254+ekaterinakrylovao@users.noreply.github.com>
2023-11-27 00:59:39 +0300

    05 done
    Программа для считывания аргументов командной строки и отображения их на экране

---
+++ labs/05/CommandLineArguments.java
@@ -0,0 +1,13 @@
+public class CommandLineArguments {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No command line arguments.");
+        } else {
+            System.out.println("Command line arguments:");
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Argument " + (i + 1) + ": " + args[i]);
+            }
+        }
+    }
+}
