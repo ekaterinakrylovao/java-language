f80857f223847ab42f865cd1d3b90f8e8a3ef1d3
ekaterinakrylovao <145141254+ekaterinakrylovao@users.noreply.github.com>
2023-11-26 22:41:40 +0300

    02 done

---
+++ labs/02/LScanner.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+
+public class LScanner {
+
+    public static void main(String[] args) {
+
+        String dir = System.getProperty("user.dir");
+
+        try {
+            Scanner fin = new Scanner(Paths.get(dir, "example.txt"), "UTF-8");
+            
+            while (fin.hasNext()) {
+                String word = fin.next();
+                System.out.println("Result = " + word);
+            }
+
+        } catch (NoSuchFileException e) {
+            System.out.println("Exception 1: " + e);
+        } catch (IOException e) {
+            System.out.println("Exception 2: " + e);
+        } catch (Exception e) {
+            System.out.println("Exception 3: " + e);
+        }
+    }
+}
