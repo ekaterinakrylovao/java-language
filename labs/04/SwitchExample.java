69f0d1b5c72297fec0e5848bcb708806fc45c35b
ekaterinakrylovao <145141254+ekaterinakrylovao@users.noreply.github.com>
2023-11-27 17:27:40 +0300

    04 version 2

--- labs/04/SwitchExample.java
+++ labs/04/SwitchExample.java
@@ -1,43 +1,97 @@
 public class SwitchExample {
-
     enum Day { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY }
 
     public static void main(String[] args) {
-        int choice = 2;
-        switchExample(choice);
-
+        byte byteValue = 2;
+        short shortValue = 222;
+        int integerValue = 222222;
         char grade = 'A';
-        switchExample(grade);
-
         String dayOfWeek = "Monday";
-        switchExample(dayOfWeek);
-
         Day enumDay = Day.WEDNESDAY;
-        switchExample(enumDay);
 
-        long longValue = 123456789L;
-        switchExample(longValue);
+        switchByte(byteValue);
+        switchShort(shortValue);
+        switchInteger(integerValue);
+        switchChar(grade);
+        switchString(dayOfWeek);
+        switchEnum(enumDay);
     }
 
-    static void switchExample(Object value) {
-        switch (value.getClass().getSimpleName()) {
-            case "Integer":
-                System.out.println("Integer value: " + value);
-                break;
-            case "Character":
-                System.out.println("Character value: " + value);
-                break;
-            case "String":
-                System.out.println("String value: " + value);
-                break;
-            case "Day":
-                System.out.println("Enum value: " + value);
-                break;
-            case "Long":
-                System.out.println("Long value: " + value);
+    static void switchByte(byte value) {
+        switch (value) {
+            case 2:
+                System.out.println("Byte variable is 2");
                 break;
             default:
-                System.out.println("Unsupported type");
+                System.out.println("Byte variable is not 2");
+        }
+    }
+
+    static void switchShort(short value) {
+        switch (value) {
+            case 2:
+                System.out.println("Short variable is 2");
+                break;
+            case 4:
+                System.out.println("Short variable is 4");
+                break;
+            case 222:
+                System.out.println("Short variable is 222");
+                break;
+            default:
+                System.out.println("There is no 'case' declared for this short value");
+        }
+    }
+
+    static void switchInteger(int value) {
+        switch (value) {
+            case 2:
+                System.out.println("Integer variable is 2");
+                break;
+            case 222222:
+                System.out.println("Integer variable is 222222");
+                break;
+            default:
+                System.out.println("There is no 'case' declared for this integer value");
+        }
+    }
+
+    static void switchChar(char value) {
+        switch (value) {
+            case 'A':
+                System.out.println("Char variable is 'A' (65)");
+                break;
+            case 'B':
+                System.out.println("Char variable is 'B' (66)");
+                break;
+            default:
+                System.out.println("There is no 'case' declared for this char value");
+        }
+    }
+
+    static void switchString(String value) {
+        switch (value) {
+            case "Sunday":
+                System.out.println("String variable is \"Sunday\"");
+                break;
+            case "Monday":
+                System.out.println("String variable is \"Monday\"");
+                break;
+            default:
+                System.out.println("There is no 'case' declared for this string value");
+        }
+    }
+
+    static void switchEnum(Day value) {
+        switch (value) {
+            case WEDNESDAY:
+                System.out.println("Enum variable is WEDNESDAY");
+                break;
+            case FRIDAY:
+                System.out.println("Enum variable is FRIDAY");
+                break;
+            default:
+                System.out.println("There is no 'case' declared for this enum value");
         }
     }
 }
